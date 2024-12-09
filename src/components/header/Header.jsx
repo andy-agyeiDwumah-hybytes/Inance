2e7bfdc9da71bdf76dcc559aa973cc272249ac35
andy.dwumah@hybytes.co.uk <andy.dwumah@hybytes.co.uk>
2024-12-09 00:24:23 +0000

    Add bootstrap and finish styling header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,49 +29,84 @@ export default function Header() {
         </Link>
       </div>
       <div className={styles.bottom}>
-        <Link to="/">Inance</Link>
-        <menu className={styles.headerMenu}>
-          <li>
-            <NavLink
-              to="/"
-              style={({ isActive }) => ({
-                color: isActive ? linkColour : "",
-              })}
+        <div className="container-fluid">
+          <nav
+            className={[
+              "navbar",
+              "navbar-expand-lg",
+              styles.nav,
+            ].join(" ")}
+          >
+            <Link to="/" className="navbar-brand">
+              <span className={styles.inanceText}>Inance</span>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              style={({ isActive }) => ({
-                color: isActive ? linkColour : "",
-              })}
+              <span className={styles.navTogglerSpan}> </span>
+            </button>
+            <div
+              className={[
+                "collapse",
+                "navbar-collapse",
+              ].join(" ")}
+              id="navbarSupportedContent"
             >
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/services"
-              style={({ isActive }) => ({
-                color: isActive ? linkColour : "",
-              })}
-            >
-              Services
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="contact"
-              style={({ isActive }) => ({
-                color: isActive ? linkColour : "",
-              })}
-            >
-              Contact Us
-            </NavLink>
-          </li>
-        </menu>
+              <menu className={["navbar-nav", styles.navbarNav].join(" ")}>
+                <li className="nav-item">
+                  <NavLink
+                    to="/"
+                    className={[styles.menuLinks, "nav-link"].join(" ")}
+                    style={({ isActive }) => ({
+                      color: isActive ? linkColour : "",
+                    })}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/about"
+                    className={[styles.menuLinks, "nav-link"].join(" ")}
+                    style={({ isActive }) => ({
+                      color: isActive ? linkColour : "",
+                    })}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/services"
+                    className={[styles.menuLinks, "nav-link"].join(" ")}
+                    style={({ isActive }) => ({
+                      color: isActive ? linkColour : "",
+                    })}
+                  >
+                    Services
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="contact"
+                    className={[styles.menuLinks, "nav-link"].join(" ")}
+                    style={({ isActive }) => ({
+                      color: isActive ? linkColour : "",
+                    })}
+                  >
+                    Contact Us
+                  </NavLink>
+                </li>
+              </menu>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
