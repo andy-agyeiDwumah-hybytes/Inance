f2cdc00170f16168ab6dabb2224033229d9cc1ca
andy.dwumah@hybytes.co.uk <andy.dwumah@hybytes.co.uk>
2024-12-10 17:37:27 +0000

    Add Slider component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,59 @@
 // Styles
 import styles from "./Header.module.css"
-// React router
-import { Link } from "react-router";
+// React
+import { Link, useLocation } from "react-router";
+import { useEffect, useState } from "react";
 // Font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+// Components
 import Navigation from "../navigation/Navigation";
+import Slider from "../slider/Slider";
 
 
 export default function Header() {
+  // Only show Slider component if current page is the index (home page)
+  const { pathname } = useLocation();
+  const [currentPageIsIndex, setCurrentPageIsIndex] = useState(true);
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setCurrentPageIsIndex(true);
+    } else {
+      setCurrentPageIsIndex(false);
+    }
+  }, [pathname]);
+
   return (
-    <header className="header_section">
-      <div className={styles.top}>
-        <div className="container-fluid">
-          <div className={["contact_nav", styles.contactNav].join(" ")}>
-            <Link to="/">
-              <FontAwesomeIcon
-                icon={faPhone}
-                className={styles.headerIcon}
-              ></FontAwesomeIcon>
-              &nbsp;Call : +01 123455678990
-            </Link>
-            <Link to="/">
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                className={styles.headerIcon}
-              ></FontAwesomeIcon>
-              &nbsp;Email : demo@gmail.com
-            </Link>
+    <>
+      <header className="header_section">
+        <div className={styles.top}>
+          <div className="container-fluid">
+            <div className={["contact_nav", styles.contactNav].join(" ")}>
+              <Link to="/">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className={styles.headerIcon}
+                ></FontAwesomeIcon>
+                &nbsp;Call : +01 123455678990
+              </Link>
+              <Link to="/">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className={styles.headerIcon}
+                ></FontAwesomeIcon>
+                &nbsp;Email : demo@gmail.com
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.bottom}>
-        <div className="container-fluid">
-          <Navigation styles={styles} />
+        <div className={styles.bottom}>
+          <div className="container-fluid">
+            <Navigation styles={styles} />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {currentPageIsIndex && <Slider />}
+    </>
   );
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,21 @@
 // React router
 import { Outlet } from "react-router";
 // Components
-import Header from "../header/Header"
-import Footer from "../footer/Footer"
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 import GetInTouch from "../getInTouch/GetInTouch";
 
 export default function Layout() {
-    return (
-      <>
+  return (
+    <>
+      <div className="hero_area">
         <Header />
-        <main>
-          <Outlet />
-        </main>
-        <GetInTouch />
-        <Footer />
-      </>
-    );
+      </div>
+      <main>
+        <Outlet />
+      </main>
+      <GetInTouch />
+      <Footer />
+    </>
+  );
 }
